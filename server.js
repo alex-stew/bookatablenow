b6e2fc55a3fa556f486a26841a03e87a99dc554a
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-02-22 20:32:37 +1030

    fix var

--- server.js
+++ server.js
@@ -68,14 +68,14 @@ app.get('/api/reservation/:reservation', (req, res) => {
 });
 
 // Create New Characters - takes in JSON input
-app.post('/api/tables', (req, res) => {
+app.post('/api/table', (req, res) => {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     const newRes = req.body;
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newRes.routeName = newRes.name.replace(/\s+/g, '').toLowerCase();
+    newRes.name = newRes.name.replace(/\s+/g, '').toLowerCase();
     console.log(newRes);
 
     reservation.push(newRes);

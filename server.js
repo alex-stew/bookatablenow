fcb991fafba9a1516c0bc13a7f85b1890520cb36
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-02-22 19:16:36 +1030

    merged changes frontend

--- server.js
+++ server.js
@@ -1,21 +1,86 @@
-// Require/import the HTTP module
-const http = require('http');
+// Dependencies
 
-// Define a port to listen for incoming requests
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
 const PORT = 3000;
 
-// Create a generic function to handle requests and responses
-const handleRequest = (request, response) => {
-    // Send the below string to the client when the user visits the PORT URL
-    response.end(`It Works!! Path Hit: ${request.url}`);
-};
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-const server = http.createServer(handleRequest);
+// Existing dinner reservations (DATA)
 
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, () => {
-    // Log (server-side) when our server has started
-    console.log(`Server listening on: http://localhost:${PORT}`);
+const reservation = [{
+        name: 'Alex',
+        email: 'alex@email.com',
+        phone: '0400 000 000',
+        id: '1234',
+    },
+    {
+        name: 'Bella',
+        email: 'bella@email.com',
+        phone: '0400 200 000',
+        id: '5678',
+    },
+    {
+        name: 'Andy',
+        email: 'andy@email.com',
+        phone: '0400 100 000',
+        id: '9765',
+    },
+];
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+// Displays all booked tables
+app.get('/api/tables', (req, res) => res.json(reservation));
+
+
+// Displays a single character, or returns false
+app.get('/api/reservation/:reservation', (req, res) => {
+    const chosen = req.params.reservation;
+
+    console.log(chosen);
+
+    /* Check each character routeName and see if the same as "chosen"
+     If the statement is true, send the character back as JSON,
+     otherwise tell the user no character was found */
+
+    for (let i = 0; i < characters.length; i++) {
+        if (chosen === characters[i].routeName) {
+            return res.json(characters[i]);
+        }
+    }
+
+    return res.json(false);
 });
+
+// Create New Characters - takes in JSON input
+app.post('/api/characters', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newCharacter = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
+    console.log(newCharacter);
+
+    characters.push(newCharacter);
+    res.json(newCharacter);
+});
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
